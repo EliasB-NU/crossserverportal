d2a7d2ce52d93b06bee41763f18fc4a345f23bc1
Elias Braun <braunelias@tghd.email>
2024-11-10 20:09:53 +0100

    initial commit

---
+++ src/client/kotlin/de/rgbpixl/CrossserverportalClient.kt
@@ -0,0 +1,9 @@
+package de.rgbpixl
+
+import net.fabricmc.api.ClientModInitializer
+
+object CrossserverportalClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
